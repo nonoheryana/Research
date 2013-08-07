33d1f592af72b35ea7ee39763c72ce7fd5c05d5c
nickhargreaves <hargreaves1234@gmail.com>
2013-08-07 12:46:19 +0300

    changed favicon

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -20,18 +20,7 @@
     <!--[if lt IE 9]>
       <script src="<?php echo base_url();?>assets/js/html5shiv.js"></script>
     <![endif]-->
-
-    <!-- Le fav and touch icons -->
-    <!-- <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo base_url();?>assets/ico/apple-touch-icon-144-precomposed.png"> -->
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo base_url();?>assets/ico/starhealth-favicon-144.png">
-    <!-- <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo base_url();?>assets/ico/apple-touch-icon-114-precomposed.png"> -->
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo base_url();?>assets/ico/starhealth-favicon-114.png">
-      <!-- <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo base_url();?>assets/ico/apple-touch-icon-72-precomposed.png"> -->
-      <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo base_url();?>assets/ico/starhealth-favicon-72.png">
-                    <!-- <link rel="apple-touch-icon-precomposed" href="<?php echo base_url();?>assets/ico/apple-touch-icon-57-precomposed.png"> -->
-                    <link rel="apple-touch-icon-precomposed" href="<?php echo base_url();?>assets/ico/starhealth-favicon-57.png">
-                                   <!-- <link rel="shortcut icon" href="<?php echo base_url();?>assets/ico/favicon.png"> -->
-                                   <link rel="shortcut icon" href="<?php echo base_url(); ?>assets/ico/starhealth-favicon-32.png">
+ <link rel="shortcut icon" href="<?php echo base_url(); ?>assets/ico/favicon.png">
   </head>
 
   <body data-spy="scroll" data-target=".bs-docs-sidebar">
